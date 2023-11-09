14434f9dd4018fc48a941f8f22493ebc8b0bc858
dmitriikalko <mitya.kalcko@yandex.ru>
2023-11-09 21:40:51 +0500

    сверстан экран профиля

---
+++ ImagesList/ProfileViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Калько on 09.11.2023.
+//
+
+import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
