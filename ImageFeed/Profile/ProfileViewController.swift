63e46a64459c18b780a05abb508a8e7406801594
dmitriikalko <mitya.kalcko@yandex.ru>
2023-11-29 13:18:03 +0500

    made profile view in code

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -10,19 +10,82 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
-    
-    //MARK: IBOutlets
-    
-    @IBOutlet weak var avatarImageView: UIImageView!
-    
-    @IBOutlet weak var loginNameLabel: UILabel!
-    @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var logoutButton: UIButton!
+   
     
     
-    //MARK: IBActions
-    
-    @IBAction func didTapLogoutButton() {
+    override func viewDidLoad() {
+        makeProfileView()
     }
+    
+    
+    private func makeProfileView() {
+        
+        //добавляем картинку профиля
+        let profileImage = UIImage(named: "PhotoUser")
+        let imageView = UIImageView(image: profileImage)
+        
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(imageView)
+        imageView.heightAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        imageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 76).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16).isActive = true
+        
+        //добавляем кнопку выхода из профиля
+        let exitProfileButton = UIButton.systemButton(
+            with: UIImage(named: "Exit")!,
+            target: self,
+            action: #selector(Self.didTapExitButton)
+        )
+        exitProfileButton.tintColor = .red
+        
+        exitProfileButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(exitProfileButton)
+        exitProfileButton.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        exitProfileButton.widthAnchor.constraint(equalToConstant: 44).isActive = true
+        exitProfileButton.centerYAnchor.constraint(equalTo: imageView.centerYAnchor).isActive = true
+        exitProfileButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16).isActive = true
+        
+        //добавляем текстовые поля
+        let nameLabel = UILabel()
+        let nickNameLabel = UILabel()
+        let descriptionLabel = UILabel()
+        
+        nameLabel.text = "Екатерина Новикова"
+        nickNameLabel.text = "@ekaterina_nov"
+        descriptionLabel.text = "Hello World!"
+        
+        nameLabel.textColor = .white
+        nameLabel.font = UIFont.systemFont(ofSize: 23, weight: .bold)
+        
+        nickNameLabel.textColor = .gray
+        nickNameLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        
+        descriptionLabel.textColor = .white
+        descriptionLabel.font = UIFont.systemFont(ofSize: 13, weight: .regular)
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nickNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(nameLabel)
+        view.addSubview(nickNameLabel)
+        view.addSubview(descriptionLabel)
+        
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 8).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        
+        nickNameLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8).isActive = true
+        nickNameLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+        
+        descriptionLabel.topAnchor.constraint(equalTo: nickNameLabel.bottomAnchor, constant: 8).isActive = true
+        descriptionLabel.leadingAnchor.constraint(equalTo: imageView.leadingAnchor).isActive = true
+    }
+    
+    @objc
+    private func didTapExitButton() {}
+    
+    
 }
