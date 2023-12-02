6be39c2802b739c2ad8d3183784a76c7312e6b87
dmitriikalko <mitya.kalcko@yandex.ru>
2023-12-02 19:36:09 +0500

    сверстаны authviewcontroller and webviewcontroller

---
+++ ImageFeed/Auth/AuthViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Калько on 02.12.2023.
+//
+
+import Foundation
+import UIKit
+
+final class AuthViewController: UIViewController {
+    
+}
---
+++ ImageFeed/Auth/WebViewController.swift
@@ -0,0 +1,13 @@
+//
+//  WebViewController.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Калько on 02.12.2023.
+//
+
+import Foundation
+import UIKit
+
+final class WebViewController: UIViewController{
+    
+}
