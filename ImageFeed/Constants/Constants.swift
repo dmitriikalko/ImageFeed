3e7f963b058fc7c19ece4c0b78a82d2ab57a8db0
dmitriikalko <mitya.kalcko@yandex.ru>
2023-12-02 17:41:44 +0500

    создан файл constants  и записаны констатны api

---
+++ ImageFeed/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ImageFeed
+//
+//  Created by Дмитрий Калько on 02.12.2023.
+//
+
+import Foundation
+
+
+let accessKey = "rgpPasAneWEWBeLevX42Ud1nSyALb_dQrCLADxO1lzY"
+let secretKey = "7uUYDEKJPEg0zDD54PQ_jioF3oMumr0QCh8ZLrHaDYY"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "Public access + Read user access + Write likes access"
+let defaultBaseURL = "https://api.unsplash.com/"
